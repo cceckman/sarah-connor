f13fa181a48247cb1383db9dac25e6d6ff696dbd
Charles Eckman <charles@cceckman.com>
2024-04-09 16:12:46 -0400

    Experiment with what calls look like

--- src/simple.c
+++ src/simple.c
@@ -1,9 +1,22 @@
 
+#include <stdlib.h>
+
 volatile int return_value = 0;
+
+int get_return_value() {
+    return return_value;
+}
 
 int main(int argc, const char** argv) {
     if(argc > 0) {
         return_value = 1;
     }
-    return return_value;
+    void *x = malloc(2);
+    return_value += (size_t)(x);
+    int v = get_return_value();
+    if(v > 0) {
+        return_value = 1;
+    }
+
+    return get_return_value();
 }
